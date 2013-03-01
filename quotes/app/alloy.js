808404ba3239b3cba96d7da697d3b40d0ded958e
Andrew Lombardi <andrew@mysticcoders.com>
2013-03-01 03:00:17 -0500

    here we go

---
+++ quotes/app/alloy.js
@@ -0,0 +1,57 @@
+// The contents of this file will be executed before any of
+// your view controllers are ever executed, including the index.
+// You have access to all functionality on the `Alloy` namespace.
+//
+// This is a great place to do any initialization for your app
+// or create any global variables/functions that you'd like to
+// make available throughout your app. You can easily make things
+// accessible globally by attaching them to the `Alloy.Globals`
+// object. For example:
+//
+// Alloy.Globals.someGlobalFunction = function(){};
+
+
+quotes = [
+    {"name": "Rasmus Lerdorf",
+    "quotes": [
+                "I did not develop the PHP we know today. Dozens, if not hundreds of people, developed PHP. I was simply the first developer.",
+                "I actually hate programming, but I love solving problems.",
+                "I really don't like programming. I built this tool to program less so that I could just reuse code.",
+                "PHP is about as exciting as your toothbrush. You use it every day, it does the job, it is a simple tool, so what? Who would want to read about toothbrushes?",
+                "I was really, really bad at writing parsers. I still am really bad at writing parsers.",
+                "We have things like protected properties. We have abstract methods. We have all this stuff that your computer science teacher told you you should be using. I don't care about this crap at all.",
+                "There are people who actually like programming. I don't understand why they like programming.",
+                "I'm not a real programmer. I throw together things until it works then I move on. The real programmers will say \"Yeah it works but you're leaking memory everywhere. Perhaps we should fix that.\" I’ll just restart Apache every 10 requests.",
+                "I do care about memory leaks but I still don\'t find programming enjoyable.",
+                "I don\'t know how to stop it, there was never any intent to write a programming language [...] I have absolutely no idea how to write a programming language, I just kept adding the next logical step on the way.",
+                "For all the folks getting excited about my quotes. Here is another - Yes, I am a terrible coder, but I am probably still better than you :)",
+                "I\'ve never thought of PHP as more than a simple tool to solve problems.",
+                "PHP is rarely the bottleneck.",
+                "Ugly problems often require ugly solutions. Solving an ugly problem in a pure manner is bloody hard."
+            ]
+    },
+    {"name": "Edsger Dijkstra",
+    "quotes": [
+                "As a result of a long sequence of coincidences I entered the programming profession officially on the first spring morning of 1952, and as far as I have been able to trace, I was the first Dutchman to do so in my country.",
+                "We must be very careful when we give advice to younger people: sometimes they follow it!",
+                "The major cause [of the software crisis] is that the machines have become several orders of magnitude more powerful! To put it quite bluntly: as long as there were no machines, programming was no problem at all; when we had a few weak computers, programming became a mild problem, and now we have gigantic computers, programming has become an equally gigantic problem. In this sense the electronic industry has not solved a single problem, it has only created them, it has created the problem of using its products.",
+                "FORTRAN's tragic fate has been its wide acceptance, mentally chaining thousands and thousands of programmers to our past mistakes.",
+                "LISP has been jokingly described as \"the most intelligent way to misuse a computer\". I think that description a great compliment because it transmits the full flavor of liberation: it has assisted a number of our most gifted fellow humans in thinking previously impossible thoughts.",
+                "When FORTRAN has been called an infantile disorder, full PL/1, with its growth characteristics of a dangerous tumor, could turn out to be a fatal disease.",
+                "If you want more effective programmers, you will discover that they should not waste their time debugging, they should not introduce the bugs to start with.",
+                "Program testing can be a very effective way to show the presence of bugs, but it is hopelessly inadequate for showing their absence.",
+                "The effective exploitation of his powers of abstraction must be regarded as one of the most vital activities of a competent programmer."
+            ]
+    },
+    {"name": "Charles Babbage",
+    "quotes": [
+                "It is therefore not unreasonable to suppose that some portion of the neglect of science in England, may be attributed to the system of education we pursue. A young man passes from our public schools to the universities, ignorant of almost every branch of useful knowledge; and at these latter establishments … classical and mathematical pursuits are nearly the sole objects proposed to the student's ambition.",
+                "If we look at the fact, we shall find that the great inventions of the age are not, with us at least, always produced in universities.",
+                "Mr. Herschel … brought with him the calculations of the computers, and we commenced the tedious process of verification. After a time many discrepancies occurred, and at one point these discordances were so numerous that I exclaimed, \"I wish to God these calculations had been executed by steam,\" to which Herschel replied, \"It is quite possible.\"",
+                "On two occasions I have been asked,—\"Pray, Mr. Babbage, if you put into the machine wrong figures, will the right answers come out?\" In one case a member of the Upper, and in the other a member of the Lower, House put this question. I am not able rightly to apprehend the kind of confusion of ideas that could provoke such a question.",
+                "The whole of arithmetic now appeared within the grasp of mechanism.",
+                "As soon as an Analytical Engine exists, it will necessarily guide the future course of the science. Whenever any result is sought by its aid, the question will then arise — by what course of calculation can these results be arrived at by the machine in the shortest time?",
+                "It is difficult to estimate the misery inflicted upon thousands of persons, and the absolute pecuniary penalty imposed upon multitudes of intellectual workers by the loss of their time, destroyed by organ-grinders and other similar nuisances."
+            ]
+    }
+];
